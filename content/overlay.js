2e5513c59113096544434f2c720e8f3ae4ae823e
SwooshyCueb <swooshycueb@tearmedia.info>
2014-06-28 03:57:37 -0500

    Some code commenting

--- content/overlay.js
+++ content/overlay.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', function()  {
-	
+// Call the startup function when loaded
+window.addEventListener('load', function()  {	
 	Absentee.startup();     
 }, false);
 
@@ -13,19 +13,22 @@ var Absentee = {
 
 	startup : function() {
 		this.browser = document.getElementById("main-window");
+		// For some reason, sizemodechange never triggers...
 		this.browser.addEventListener('sizemodechange', function () {
 			console.log('sizemodechange event triggered. If this is working, we do not need to monitor resize events any more.')
 		}, false);
-		//For some reason, sizemodechange never triggers...
+		// Call the onsizemodechange function when the window is resized
 		this.browser.addEventListener('resize', function () {
 			Absentee.onsizemodechange();
 		}, false);
+		// Run it manually once to ensure everything's right from the get-go
 		this.onsizemodechange();
 		this.tabview = document.getElementById("tabbrowser-tabs");
-		//It's very possible for the attribute to not exist yet, so we'll create it if it doesn't.
+		// It's very possible for the attribute to not exist yet, so we'll create it if it doesn't.
 		if(!this.tabview.hasAttribute("overflow")) {
 			this.tabview.setAttribute("overflow", "");
 		}
+		// Call ontaboverflow when tab overflow status changes
 		var taboverflowmonitor = new MutationObserver(function() {
 			Absentee.ontaboverflow();
 		});
@@ -34,14 +37,16 @@ var Absentee = {
 	},
 
 	onsizemodechange : function () {
+		// Detect new window state and set attributes appropriately
 		if (this.winstate === window.windowState) {
+			// Did the window state actually change?
 		} else if (window.windowState === 1) {
 			this.browser.setAttribute("abs-winstate", "maximized");
 			this.winstate = window.windowState;
 		} else if (window.windowState === 2) {
 			//this.browser.setAttribute("abs-winstate", "minimized");
 			//this.winstate = window.windowState;
-			//don't update, we don't care
+			//We don't care if the window is minimized.
 		} else if (window.windowState === 3) {
 			this.browser.setAttribute("abs-winstate", "normal");
 			this.winstate = window.windowState;
@@ -52,6 +57,7 @@ var Absentee = {
 	},
 
 	ontaboverflow : function () {
+		// It's very unliekyl that we'd end up here without overflow changing
 		if(this.tabview.getAttribute("overflow") == "true") {
 			this.browser.setAttribute("tab-overflow", "true");
 		} else {
@@ -60,6 +66,8 @@ var Absentee = {
 	},
 
 	maximize : function() {
+		// Replacement maximize/restore function for the maximize/restore button
+		// (By default, the button Firefox provides only toggles fullscreen)
 		if (window.windowState === 1) {
 			window.restore();
 		} else if (window.windowState === 2) {
